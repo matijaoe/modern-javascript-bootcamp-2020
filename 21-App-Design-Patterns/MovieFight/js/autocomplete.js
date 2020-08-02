91759c02503d142fa732da9212eda62de8e3dd99
Matija Osrecki <matija.osrecki@gmail.com>
2020-08-02 22:50:04 +0200

    refactored and made autocomplete reusable :wrench:

---
+++ 21-App-Design-Patterns/MovieFight/js/autocomplete.js
@@ -0,0 +1,74 @@
+const createAutocomplete = ({
+    root,
+    renderOption,
+    onOptionSelect,
+    inputValue,
+    fetchData
+}) => {
+
+    // create HTML for autocomplete widget
+    root.innerHTML = `
+    <label><b>Search</b></label>
+    <input class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
+    // get elements from HTML root
+    const input = root.querySelector('input');
+    const dropdown = root.querySelector('.dropdown');
+    const resultsWrapper = root.querySelector('.results');
+
+    // action to do after getting whole value of input
+    const onInput = async event => {
+        const items = await fetchData(event.target.value);
+
+        // clear data on new input
+        resultsWrapper.innerHTML = '';
+
+        // if no items found
+        if (!items.length) {
+            dropdown.classList.remove('is-active');
+            return;
+        }
+
+        // open the dropdown
+        dropdown.classList.add('is-active');
+
+        for (let item of items) {
+            const option = document.createElement('a');
+
+            option.classList.add('dropdown-item');
+            // generate HTML for the option
+            option.innerHTML = renderOption(item);
+
+            option.addEventListener('click', () => {
+                dropdown.classList.remove('is-active');
+                input.value = inputValue(item);
+
+                // fetch more elaborate details
+                onOptionSelect(item);
+            });
+
+            resultsWrapper.append(option);
+        }
+    };
+
+    // on each input change, pass our custom fetch action to debounce function
+    input.addEventListener('input', debounce(onInput, 500));
+
+    document.addEventListener('click', event => {
+        // close the dropdown when clicked outside root
+        if (!root.contains(event.target)) {
+            dropdown.classList.remove('is-active');
+        } else if (input.contains(event.target)) {
+            // reopen the dropdown when clicking on input if dropdown has data
+            if (resultsWrapper.querySelector('a')) {
+                dropdown.classList.add('is-active');
+            };
+        }
+    });
+};
---
+++ 21-App-Design-Patterns/MovieFight/js/index.js
@@ -0,0 +1,107 @@
+// create autocomplete widget
+createAutocomplete({
+    // where to render autocomplete to
+    root: document.querySelector('.autocomplete'),
+
+    // how to show individual item
+    renderOption(movie) {
+        const imgPlaceholder = 'https://semantic-ui.com/images/wireframe/image.png';
+        const imgSrc = movie.Poster === 'N/A' ? imgPlaceholder : movie.Poster;
+        console.log(movie);
+
+        return `
+            <img src="${imgSrc}">
+            ${movie.Title} (${movie.Year})
+        `;
+    },
+ 
+    // what to do when someone click on the item
+    onOptionSelect(movie) {
+        // fetch more elaborate movie details
+        onMovieSelect(movie);
+    },
+
+    // what to backfill in the input when someobody clicks on the item
+    inputValue(movie) {
+        return movie.Title;
+    },
+
+    // how to fetch the data
+    async fetchData(searchTerm) {
+        const response = await axios.get('http://www.omdbapi.com/', {
+            params: {
+                apikey: 'ee59ae23',
+                s: searchTerm
+            }
+        });
+
+        if (response.data.Error) {
+            console.log('No movies found');
+            return [];
+        }
+
+        return response.data.Search;
+    }
+});
+
+// function to fetch elaborate data for the movie
+const onMovieSelect = async movie => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'ee59ae23',
+            i: movie.imdbID
+        }
+    });
+    console.log(response.data);
+
+    document.querySelector('.summary').innerHTML = movieTemplate(response.data);
+};
+
+// return HTML for movie stats
+const movieTemplate = (movieDetail) => {
+    return `
+        <article class="media">
+            <figure class="media-left">
+                <p class="image">
+                    <img src="${movieDetail.Poster}" alt="Movie Poster">
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <h6>
+                        <span>${movieDetail.Country.split(',')[0]}</span>, 
+                        <span>${movieDetail.Year}</span>
+                    </h6>
+                    <p>${movieDetail.Plot}</p>
+                </div>
+            </div>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Awards}</p>
+            <p class="subtitle">Awards</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.BoxOffice || 'idk tbh 🤷‍♂️'}</p>
+            <p class="subtitle">Box Office</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Ratings[1] ? movieDetail.Ratings[1].Value : 'N/A'}</p>
+            <p class="subtitle">Rotten Tomatoes</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Metascore}</p>
+            <p class="subtitle">Metascore</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbRating}</p>
+            <p class="subtitle">IMDB Rating</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbVotes}</p>
+            <p class="subtitle">IMBD Votes</p>
+        </article>
+    `;
+};
+

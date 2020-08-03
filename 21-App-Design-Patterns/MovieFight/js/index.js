9642d846de00ec205ef4e6d706b9cff6d3cb0f16
Matija Osrecki <matija.osrecki@gmail.com>
2020-08-03 15:18:24 +0200

    built comparison algorithym :wrench:

--- 21-App-Design-Patterns/MovieFight/js/index.js
+++ 21-App-Design-Patterns/MovieFight/js/index.js
@@ -3,7 +3,6 @@ const autoCompleteConfig = {
     renderOption(movie) {
         const imgPlaceholder = 'https://semantic-ui.com/images/wireframe/image.png';
         const imgSrc = movie.Poster === 'N/A' ? imgPlaceholder : movie.Poster;
-        console.log(movie);
 
         return `
             <img src="${imgSrc}">
@@ -41,7 +40,7 @@ createAutocomplete({
     // what to do when someone click on the item
     onOptionSelect(movie) {
         // fetch more elaborate movie details
-        onMovieSelect(movie, document.querySelector('#left-summary'));
+        onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
         document.querySelector('.tutorial').classList.add('is-hidden');
     },
 
@@ -53,28 +52,102 @@ createAutocomplete({
 createAutocomplete({
     root: document.querySelector('#right-autocomplete'),
     onOptionSelect(movie) {
-        onMovieSelect(movie, document.querySelector('#right-summary'));
+        onMovieSelect(movie, document.querySelector('#right-summary'), 'right');
         document.querySelector('.tutorial').classList.add('is-hidden');
     },
     ...autoCompleteConfig
 });
 
+let leftMovie;
+let rightMovie;
 // function to fetch elaborate data for the movie
-const onMovieSelect = async (movie, summaryElement) => {
+const onMovieSelect = async (movie, summaryElement, side) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: 'ee59ae23',
             i: movie.imdbID
         }
     });
-    console.log(response.data);
+    // print to console data for the movie
+    console.log(`%c${response.data.Title} (${response.data.Year})`, 'font-size: 18px', response.data);
 
     summaryElement.innerHTML = movieTemplate(response.data);
+
+    if (side === 'left') {
+        leftMovie = response.data;
+    } else if (side === 'right') {
+        rightMovie = response.data;
+    }
+
+    // run comparison when both movies are defined
+    if (leftMovie && rightMovie) {
+        runComparison();
+    }
 };
+
+const runComparison = () => {
+    const leftSideStats = document.querySelectorAll('#left-summary .notification');
+    const rightSideStats = document.querySelectorAll('#right-summary .notification');
+
+    leftSideStats.forEach((leftStat, index) => {
+        const rightStat = rightSideStats[index];
+
+        const leftSideValue = +leftStat.dataset.value;
+        const rightSideValue = +rightStat.dataset.value;
+
+        console.log(leftSideValue, rightSideValue);
+        console.log('leftSideValue > rightSideValue', leftSideValue > rightSideValue);
+        console.log('leftSideValue < rightSideValue', leftSideValue < rightSideValue);
+        console.log('leftSideValue == rightSideValue', leftSideValue == rightSideValue);
+
+
+        if (leftSideValue > rightSideValue) {
+            // TODO classes stays
+
+            // remove leftovers
+            // leftStat.classList.remove('is-warning');
+            rightStat.classList.remove('is-primary');
+
+            leftStat.classList.add('is-primary');
+            // rightStat.classList.add('is-warning');
+
+        } else if (leftSideValue < rightSideValue) {
+
+            leftStat.classList.remove('is-primary');
+            // rightStat.classList.remove('is-warning');
+
+            // leftStat.classList.add('is-warning');
+            rightStat.classList.add('is-primary');
+        } else {
+            // leftStat.classList.remove('is-warning');
+            leftStat.classList.remove('is-primary');
+            rightStat.classList.remove('is-primary');
+            // rightStat.classList.remove('is-warning');
+        }
+
+    });
+}
 
 // return HTML for movie stats
 const movieTemplate = (movieDetail) => {
-    // TODO fix Rotten tomatoes (The Gentlemen) - fixed but needs refactor
+    const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, '')) || 0;
+    const metascore = parseInt(movieDetail.Metascore) || 0;
+    const imdbRating = parseFloat(movieDetail.imdbRating) || 0;
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, '')) || 0;
+
+    // console.log(metascore, imdbRating, imdbVotes);
+
+    const awards = movieDetail.Awards.split(' ').reduce((total, curr) => {
+        let value = parseInt(curr);
+
+        if (isNaN(value)) {
+            return total;
+        }
+        return total + value;
+    }, 0);
+    console.log(awards);
+
+    // TODO fix Rotten tomatoes
     return `
         <article class="media">
             <figure class="media-left">
@@ -87,39 +160,43 @@ const movieTemplate = (movieDetail) => {
                     <h1>${movieDetail.Title}</h1>
                     <h4>${movieDetail.Genre}</h4>
                     <h6>
-                        <span>${movieDetail.Country.split(',')[0]}</span>, 
-                        <span>${movieDetail.Year}</span>
+                    <span class="tag is-primary is-light">${movieDetail.Year}</span>
+                        <span class="tag is-link is-light">${movieDetail.Country.split(',')[0]}</span> 
+                        <span class="tag is-danger is-light">${movieDetail.Rated}</span>
                     </h6>
                     <p>${movieDetail.Plot}</p>
                 </div>
             </div>
         </article>
-        <article class="notification is-primary">
+
+        <article data-value=${awards} class="notification">
             <p class="title">${movieDetail.Awards}</p>
             <p class="subtitle">Awards</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value=${dollars} class="notification">
             <p class="title">${movieDetail.BoxOffice || 'idk tbh 🤷‍♂️'}</p>
             <p class="subtitle">Box Office</p>
         </article>
-        <article class="notification is-primary">
-            <p class="title">${movieDetail.Ratings[1] ?
-            movieDetail.Ratings[1].Source === 'Rotten Tomatoes' ?
-                movieDetail.Ratings[1].Value : 'N/A' : 'N/A'}</p>
-            <p class="subtitle">Rotten Tomatoes</p>
-        </article>
-        <article class="notification is-primary">
+        <article data-value=${metascore} class="notification">
             <p class="title">${movieDetail.Metascore}</p>
             <p class="subtitle">Metascore</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value=${imdbRating} class="notification">
             <p class="title">${movieDetail.imdbRating}</p>
             <p class="subtitle">IMDB Rating</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value=${imdbVotes} class="notification">
             <p class="title">${movieDetail.imdbVotes}</p>
             <p class="subtitle">IMBD Votes</p>
         </article>
     `;
 };
 
+/*
+<article class="notification is-primary">
+            <p class="title">${movieDetail.Ratings[1] ?
+            movieDetail.Ratings[1].Source === 'Rotten Tomatoes' ?
+                movieDetail.Ratings[1].Value : 'N/A' : 'N/A'}</p>
+            <p class="subtitle">Rotten Tomatoes</p>
+        </article>
+*/

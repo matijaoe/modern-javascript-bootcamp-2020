23b0160dc24a788c87f57e8a5478a6c716853077
Matija Osrecki <matija.osrecki@gmail.com>
2020-09-07 19:21:02 +0200

    added mocha implemenatation :goal_net:

--- 32-Testing/hidash/index-our.test.js
+++ 32-Testing/hidash/index-our.test.js
@@ -1,17 +1,29 @@
 const { forEach, map } = require('./index');
 const assert = require('assert').strict;
 
+const test = (desc, fn) => {
+    console.log('----', desc);
 
-it('The forEach function', () => {
+    try {
+        fn();
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+test('The forEach function', () => {
     let sum = 0;
     forEach([1, 2, 3], value => {
         sum += value;
     })
 
+    // message is optional
     assert.strictEqual(sum, 6, 'Expected forEach to sum the array')
+
+    // if (sum !== 6) throw new Error('Expected summing array to equal 6');
 });
 
-it('The map function', () => {
+test('The map function', () => {
     const result = map([1, 2, 3], value => {
         return value * 2;
     });
